83f7de4c234ac9ddadec1fd57b8c357a9c4e8006
Natanael Pacheco <natepacheco716@gmail.com>
2023-01-21 18:56:05 -0600

    Implemented theme for sanity studio.

--- sanity.config.js
+++ sanity.config.js
@@ -1,18 +1,28 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+import { data } from "autoprefixer";
+import { myTheme } from "./theme";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineConfig({
-  name: 'default',
-  title: 'reality-labs',
+  basePath: "/studio", // This is where we're rendering it on the site
+  name: "Reality_Labs_Content_Studio",
+  title: "Reality Labs Content Studio",
+  projectId,
+  dataset,
 
-  projectId: 'rmcrmm8g',
-  dataset: 'production',
-
+  //Vision Tool allows us to test our queries on the backend
+  //Desk Tool is what allows us to add information in the format shown
   plugins: [deskTool(), visionTool()],
+
+  //These are being renderedfrom the schema folder, these allows us
 
   schema: {
     types: schemaTypes,
   },
-})
+  theme: myTheme,
+});
