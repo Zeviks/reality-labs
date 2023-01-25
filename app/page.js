079930130733b3a21245b99fb8d2da6d0ba494ea
Natanael Pacheco <natepacheco716@gmail.com>
2023-01-24 22:38:24 -0600

    Added page gradients

--- app/page.js
+++ app/page.js
@@ -12,18 +12,29 @@ import {
   Partners,
 } from "../sections";
 
+//in order to add multiple gradient, i had to devide the page into multiple and inbetween ad divs
+
 const Page = () => (
   <div className="bg-primary-black text-white overfill-hidden">
     <Navbar />
     <Hero />
     <Partners />
-    <About />
-    <Explore />
-    <GetStarted />
-    <WhatsNew />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
+      <Explore />
+    </div>
+    <div className="relative">
+      <GetStarted />
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
+    </div>
     <World />
-    <Insights />
-    <Feedback />
+    <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback />
+    </div>
     <Footer />
   </div>
 );
